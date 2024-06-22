52b98390eab1dd9f22589d444a4e6cc1a8558324
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2024-06-23 01:01:25 +0800

    penambahan mentor template

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mentor.mentor');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
